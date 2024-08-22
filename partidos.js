d8ab5043b208e9b69d0ab8b8c91cf2f343fcbdde
JuanaTib07 <163531889+JuanaTib07@users.noreply.github.com>
2024-08-22 09:57:52 -0300

    update partidos y intente get participantes

--- partidos.js
+++ partidos.js
@@ -15,6 +15,7 @@ const createPartido = async (req, res) => {
 }
 catch (e) {
     res.status(500).json({ error: 'Ya hay un partido registrado con estos datos'});
+    await client.end();
 }
 };
 
@@ -32,6 +33,7 @@ const deletePartido = async (req, res) => {
     }
     catch (e) {
         res.json ({error: 'El partido no pudo ser eliminado correctamente'});
+        await client.end();
     }
 
 }
@@ -49,10 +51,42 @@ const getPartido = async (req, res) => {
     }
     catch (e) {
         res.status(500).json({ error: 'No se puede mostrar el partido' });
+        await client.end();
     }
 
 }
 
 
+const updatePartido = async (req, res) =>{
+    const cliente = new Cliente(config);
+    try{
+        await conn.query(
+            `UPDATE partido SET hora = ?, fecha = ? cancha id = ?`,
+            [req.body.hora, req.body.fecha, req.body.cancha]
+        );
+        res.json({ id: req.params.id });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+        await client.end();
+    }
+
+}
+
+
+const getParticipantes = async (req, res) => {
+    const { id_partido } = req.params;
+    const client = new Client(config);
+    try {
+        const result = await client.query(
+            `SELECT i.usuario, email FROM inscripciones i JOIN usuarios u ON i.id_usuario = u.id WHERE i.id_partido = $1`,
+            [id_partido]
+        );
+        res.json(result.rows);
+    } catch (e) {
+        res.status(500).json({ error: 'No se pudieron obtener los participantes' });
+    } finally {
+        client.end();
+    }
+};
 
 
