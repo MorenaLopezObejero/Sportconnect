ab9a8dbf19fb9b26ce37670fcf80697073d7a6c5
JuanaTib07 <163531889+JuanaTib07@users.noreply.github.com>
2024-09-05 08:36:01 -0300

    puse lo que funciona de las rutas

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.post("/sport", regisyinises.register);
 
 
 //partidos 
-app.post("/createPartido", partido.createPartido);
-app.delete("/deletePartido/id", partido.deletePartido);
+app.post("/createPartido", partido.createPartido); //listo 
+app.delete("/deletePartido/:id", partido.deletePartido); //listo
 app.get("/getPartido", partido.getPartido); //listo
 app.put("/updatepartido", partido.updatePartido);
 app.get("/getPartidoDate", partido.getPartido) //listo
-app.post("/createCancha", cancha.createCancha);
+app.post("/createCancha", cancha.createCancha); //listo
 app.get("/getCancha", cancha.getCancha); //listo
-app.delete("/deleteCancha/:id", cancha.deleteCancha);
+app.delete("/deleteCancha/:id", cancha.deleteCancha); //listo
 
 
 
