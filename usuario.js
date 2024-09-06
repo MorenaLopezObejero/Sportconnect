888d31915cd71561e3147fe76098fae25fbee8ee
MorenaLopezObejero <48111548@est.ort.edu.ar>
2024-09-06 09:34:49 -0300

    agrege bcryptjs

--- usuario.js
+++ usuario.js
@@ -3,17 +3,21 @@ import pkg from 'pg';
 const {Client} = pkg;
 const client = new Client(config);
 await client.connect();
+import bcrypt from "brycrptjs"
 
 const register = async (req,res) => {
-    const {email, foto_perfil, genero, nombre, fecha_nacimiento, zona, contraseña} = req.body;
+    const {email, foto_perfil, genero, nombre, fecha_nacimiento, zona, contrasena} = req.body;
     try {
         const emailquery = 'SELECT email FROM usuario WHERE email = ?'
         const emailValues =  [email];
         const result = await client.query(emailquery, emailValues);
 
+        const contrahashed = await bcrypt.hash(contrasena, 10);
+        console.loge(contrahashed);
+
         if (result.rows [0] = null){
-            const query = 'INSERT INTO usuario (email, foto_perfil, genero, nombre, fecha_nacimiento, zona, contraseña) values ($1, $2, $3, $4, $5, $6, $7)'
-            
+            const query = 'INSERT INTO usuario (email, foto_perfil, genero, nombre, fecha_nacimiento, zona, contrasena) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *'
+            const result = await config.query(query, [email, foto_perfil, genero, nombre, fecha_nacimiento, zona, contrahashed])
         }
         else{
             res.json ({error: 'Ya hay una cuenta registrada con ese email'})
