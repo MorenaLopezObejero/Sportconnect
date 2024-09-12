a31e91d2a86bf9ded7ce32a0209a6e4d7d842309
MorenaLopezObejero <48111548@est.ort.edu.ar>
2024-09-12 10:07:29 -0300

    cambie client y config

--- debugconfig.js
+++ debugconfig.js
@@ -1,8 +1,13 @@
-export const config = {
+import postgresql from 'pg';
+const { config } = postgresql;
+
+const client = new config({
     user: "default",
     host: "ep-little-shadow-a4frsyb1.us-east-1.aws.neon.tech",
     database: "verceldb",
     password: "jY9dHtM6OUBi",
     port: 5432,
     ssl: true
-}
+});
+
+export default client;
--- usuario.js
+++ usuario.js
@@ -1,19 +1,20 @@
-import {config} from './debugconfig.js';
+/*import {config} from './debugconfig.js';
 import pkg from 'pg';
 const {Client} = pkg;
 const client = new Client(config);
 await client.connect();
-import bcrypt from "brycrptjs"
+*/
+import config from '../debugconfig.js'
+import bcrypt from "bcryptjs"
 
 const register = async (req,res) => {
     const {email, foto_perfil, genero, nombre, fecha_nacimiento, zona, contrasena} = req.body;
     try {
         const emailquery = 'SELECT email FROM usuario WHERE email = ?'
-        const emailValues =  [email];
-        const result = await client.query(emailquery, emailValues);
+        const result = await config.query(emailquery, [email]);
 
         const contrahashed = await bcrypt.hash(contrasena, 10);
-        console.loge(contrahashed);
+        console.log(contrahashed);
 
         if (result.rows [0] = null){
             const query = 'INSERT INTO usuario (email, foto_perfil, genero, nombre, fecha_nacimiento, zona, contrasena) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *'
@@ -25,7 +26,7 @@ const register = async (req,res) => {
     }
     catch (err) {
         console.error(err);
-        await client.end();
+        await config.end();
         res.json ({error: 'Hubo un error al registrarse'});
     }
 };
