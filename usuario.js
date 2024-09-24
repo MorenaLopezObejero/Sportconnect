4ced0a7bdec21ad923115306865cd7fd5df9998f
MorenaLopezObejero <48111548@est.ort.edu.ar>
2024-09-24 14:13:36 -0300

    middelwares arreglados

--- usuario.js
+++ usuario.js
@@ -32,8 +32,34 @@ const register = async (req,res) => {
 };
 
 const inicioses = async (req, res) => {
-
-};
+try{
+    const Usuario = await pool.query(
+        'SELECT mail, contrasena FROM perfil WHERE mail = $1',
+        [req.body.mail]
+        );
+    if (Usuario.rows.length == 1) {
+         if (await bcrypt.compare(req.body.contrasena, Usuario.rows[0].contrasena))
+            return res.status(200).json({ message: 'Se logeo correctamente.' });
+        else
+            return res.status(500).json({ success: false, message: 'La contraseña o el mail son incorrecto11'});
+    } else
+        return res.status(500).json({ success: false, message: 'La contraseña o el mail son incorrecto2' });
+    }
+ catch (e) {
+    console.log(e);
+    res.status(500).json('La contraseña o el usuario no son correctos');
+    }
+    if(!Usuario){
+        return res.status(400).send({status:"Error", message: "Error durante el login"})
+    }
+        
+    const token = jwt.sign({ id: usuario.id }, "tu_secreto"/*process.env.SECRET*/,{
+    expiresIn: "1d",
+    });
+        
+    res.json({ token });       
+    
+}
 
 const regisyinises = {
     register,
