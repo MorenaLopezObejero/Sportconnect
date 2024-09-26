4e586e1b0e619414f418d55ee88d8df4cab2c57f
MorenaLopezObejero <48111548@est.ort.edu.ar>
2024-09-26 14:25:41 -0300

    arregle todo

--- usuario.js
+++ usuario.js
@@ -34,27 +34,29 @@ const register = async (req,res) => {
 const inicioses = async (req, res) => {
     try{
         const Usuario = await client.query(
-            'SELECT email, contrasena FROM perfil WHERE email = $1',
+            'SELECT email, contrasena FROM usuario WHERE email = $1',
             [req.body.email]
         );
 
         if (Usuario.rows.length == 1) {
-            if (await bcrypt.compare(req.body.contrasena, Usuario.rows[0].contrasena))
-                return res.status(200).json({ message: 'Se logeo correctamente.' });
+            if (await bcrypt.compare(req.body.contrasena, Usuario.rows[0].contrasena)){
+                const token = jwt.sign({ email: Usuario.email }, "el_secreto"/*process.env.SECRET*/,{
+                    expiresIn: "1d",
+                });
+                return res.status(200).json({ message: 'Se logeo correctamente.', token });
+            }     
             else
                 return res.status(500).json({ success: false, message: 'La contraseña o el mail son incorrecto11'});
         } else {
             return res.status(500).json({ success: false, message: 'La contraseña o el mail son incorrecto2' });
         }
-    }catch (e) {
+
+    }   
+        catch (e) {
         console.log(e);
         res.status(500).json('La contraseña o el usuario no son correctos3');
-    }   
-    const token = jwt.sign({ email: Usuario.email }, "tu_secreto"/*process.env.SECRET*/,{
-        expiresIn: "1d",
-    });
-        
-    res.json({ token });        
+    }      
+
 }
 
 const regisyinises = {
